86ada9a399032fc9807bdb068bf512cd1392e1d1
Mattia Zingaretti <zingaretti.mattia@gmail.com>
2022-07-19 04:38:45 +0200

    working on ui

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -3,7 +3,8 @@
 #define MAX_ITEM_SIZE 256
 #define MAX_ELEM_SIZE 32
 
-
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 	4
 
 char* procDataToString(ProcData* data){
     char* buf =(char*) calloc(MAX_ITEM_SIZE, sizeof(char));
@@ -65,45 +66,47 @@ char* procDataToString(ProcData* data){
     return buf;
 }
 
-ITEM ** getItems(int rows , int cols){
+ITEM ** getItems(){
 
     ListHead head;    
     List_init(&head);
     getAllProcData(&head);
     int listSize = head.size;
 
+    int counter = 0;
     ITEM** listItem = (ITEM**) malloc(sizeof(ITEM*)*listSize);
 
     ListItem* it = head.first;
+   
     while(it){
         ProcListItem* elem = (ProcListItem*) it;
         ProcData* pd = elem->data;
-
-        //printw("%s\n",procDataToString(pd));
-        //printw("%d\t%s\t%c\t%lu\t%lu\t%ld\t%llu\t%lu\n",(int)pd->pid, pd->comm, pd->state, pd->utime, pd->stime, pd->num_threads,pd->starttime, pd->vsize);
         
+        listItem[counter] = new_item(procDataToString(pd),"");
+        
+        counter++;
         it=it->next;
     }
 
-    return NULL;
+    return listItem;
 }
 
-
+/*
 int main(int argc, char const *argv[])
 {
 
 
     //INIT
     initscr(); //Magic function that initialize window and curses mode.
-    cbreak(); /*
+    cbreak(); 
             Disable Line buffering for the terminal. In this way no need to wait for char '\n'.
             'cbreak()' -> CTRL+C passed to the program  making signal.  
             'raw()' -> CTRL+C passed to the program WITHOUT making signal.
-            */
+            
     noecho(); //Disable immediate echoing and enable it when needed.
    	keypad(stdscr, TRUE); //It enables the reading of function keys like F1, F2, arrow keys etc
     curs_set(0);
-    /*
+    
     UTILS:
     'getch()'
     The function getch() is used to get a character from user. 
@@ -139,7 +142,7 @@ int main(int argc, char const *argv[])
         Free the memory allocated to the items with free_item()
         End curses 
 
-    */
+    
     
     int row, col;
     getmaxyx(stdscr,col,row);	
@@ -152,7 +155,6 @@ int main(int argc, char const *argv[])
     refresh();
     wrefresh(menu_win);
     keypad(menu_win,TRUE);
-    wgetch(menu_win);
 
     int highlight = 0;
     int choice;
@@ -162,22 +164,168 @@ int main(int argc, char const *argv[])
     while(1){
         for (int i = 0; i < 3; i++){
             if(i == highlight)
-                wattr_on(menu_win , A_REVERSE);
+                wattr_on(menu_win , WA_REVERSE, NULL);
             mvwprintw(menu_win,i+1 , 1, choices[i]);
-            wattr_off(menu_win , A_REVERSE);
-            
+            wattr_off(menu_win , WA_REVERSE, NULL);
         }
+        choice = wgetch(menu_win);
+
+        switch (choice)
+        {
+        case KEY_UP:
+            highlight--;
+            if(highlight == -1)
+                highlight = 0;
+            break;
+        case KEY_DOWN:
+            highlight++;
+            if(highlight == 3)
+                highlight = 2;
+            break;
         
+        default:
+            break;
+        }
+        if(choice == 10){
+            break;
+        }
     }
+
+    printw("Your choice: %s", choices[highlight]);
 
     //mainList = new_menu(getItems(row, col));
     
     //post_menu(mainList);
-    //refresh();
+    refresh();
 
     //CLEAN-UP
     getch();
     endwin(); //Disable curses mode
 
     return 0;
+}
+*/
+
+
+
+char *choices[] = {
+                        "Choice 1",
+                        "Choice 2",
+                        "Choice 3",
+                        "Choice 4",
+			"Choice 5",
+			"Choice 6",
+			"Choice 7",
+			"Choice 8",
+			"Choice 9",
+			"Choice 10",
+                        "Exit",
+                        (char *)NULL,
+                  };
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
+
+int main(){
+    
+	int c;				
+	MENU *my_menu;
+    WINDOW *my_menu_win;
+    int n_choices, i;
+	
+	/* Initialize curses */
+	initscr();
+	start_color();
+        cbreak();
+        noecho();
+	keypad(stdscr, TRUE);
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_CYAN, COLOR_BLACK);
+
+	/* Create items */
+        n_choices = ARRAY_SIZE(choices);
+        ITEM** my_items = (ITEM **)calloc(n_choices, sizeof(ITEM *));
+        for(i = 0; i < n_choices; ++i)
+                my_items[i] = new_item(choices[i], choices[i]);
+
+	/* Crate menu */
+	my_menu = new_menu((ITEM **)my_items);
+
+	/* Create the window to be associated with the menu */
+       
+    int row, col;
+    getmaxyx(stdscr,col,row);	
+        my_menu_win = newwin((int)col*0.9 , row , (col-col*0.9)/2 ,0);
+        keypad(my_menu_win, TRUE);
+     
+	/* Set main window and sub window */
+        set_menu_win(my_menu, my_menu_win);
+        set_menu_sub(my_menu, derwin(my_menu_win, col*0.7 , row*0.5, 10, 10));
+	set_menu_format(my_menu, 50 ,50);
+			
+	/* Set menu mark to the string " * " */
+        set_menu_mark(my_menu, " * ");
+
+	/* Print a border around the main window and print a title */
+        box(my_menu_win, 0, 0);
+	print_in_middle(my_menu_win, 1, 0, 40, "My Menu", COLOR_PAIR(1));
+	mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
+	mvwhline(my_menu_win, 2, 1, ACS_HLINE, 100);
+	mvwaddch(my_menu_win, 2, 39, ACS_RTEE);
+        
+	/* Post the menu */
+	post_menu(my_menu);
+	wrefresh(my_menu_win);
+	
+	attron(COLOR_PAIR(2));
+	mvprintw(LINES - 2, 0, "Use PageUp and PageDown to scoll down or up a page of items");
+	mvprintw(LINES - 1, 0, "Arrow Keys to navigate (F1 to Exit)");
+	attroff(COLOR_PAIR(2));
+	refresh();
+
+	while((c = wgetch(my_menu_win)) != KEY_F(1))
+	{       switch(c)
+	        {	case KEY_DOWN:
+				menu_driver(my_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(my_menu, REQ_UP_ITEM);
+				break;
+			case KEY_NPAGE:
+				menu_driver(my_menu, REQ_SCR_DPAGE);
+				break;
+			case KEY_PPAGE:
+				menu_driver(my_menu, REQ_SCR_UPAGE);
+				break;
+		}
+                wrefresh(my_menu_win);
+	}	
+
+	/* Unpost and free all the memory taken up */
+        unpost_menu(my_menu);
+        free_menu(my_menu);
+        for(i = 0; i < n_choices; ++i)
+                free_item(my_items[i]);
+	endwin();
+}
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color)
+{	int length, x, y;
+	float temp;
+
+	if(win == NULL)
+		win = stdscr;
+	getyx(win, y, x);
+	if(startx != 0)
+		x = startx;
+	if(starty != 0)
+		y = starty;
+	if(width == 0)
+		width = 80;
+
+	length = strlen(string);
+	temp = (width - length)/ 2;
+	x = startx + (int)temp;
+	wattron(win, color);
+	mvwprintw(win, y, x, "%s", string);
+	wattroff(win, color);
+	refresh();
 }
